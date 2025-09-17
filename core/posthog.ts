2a4449c962316499c5c5c580999ac7f70d7699ad
ri72miieop <106687171+ri72miieop@users.noreply.github.com>
2025-09-17 18:00:15 +0100

    always create a profile for analytics

--- core/posthog.ts
+++ core/posthog.ts
@@ -8,7 +8,7 @@ import posthog from 'posthog-js/dist/module.no-external'
 posthog.init(process.env.PLASMO_PUBLIC_POSTHOG_API_KEY,
     {
         api_host: 'https://us.i.posthog.com',
-        person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+        person_profiles: 'always' // or 'always' to create profiles for anonymous users as well
     }
 )
 
